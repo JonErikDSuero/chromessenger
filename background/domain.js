827b1c56a498e4cfdd2b51611133cc73101a407f
JonErikDSuero <jonerikdsuero@gmail.com>
2015-01-16 21:57:37 -0500

    first commit

---
+++ background/domain.js
@@ -0,0 +1,15 @@
+var CME_domain = (function () {
+
+  //var domain = 'http://localhost:3000/';
+  var domain = 'http://chromessengerbackend.prombly.com/';
+
+  return {
+
+    url: function(str) {
+      return domain+str;
+    } // url
+
+  };
+
+}());
+
