96a452e002c3de7d6baea01fa1cf830f5e14509c
kumarrus <rushab3737@gmail.com>
2015-01-17 21:17:32 -0500

    Add localstorage to extract lists for webpages and store.

--- background/background.js
+++ background/background.js
@@ -2,8 +2,10 @@
 chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
 	if(request.method == "setLocalStorage"){
 		//localStorage["value1"] = request.value;
-		//console.log("Recieved message");
-		sendResponse({data: true});
+		console.log("Recieved message");
+		localStorage[request.key]=request.data;
+		console.log(localStorage);
+		sendResponse(localStorage[request.key]);
 	} else {
 		sendResponse({});
 	}
