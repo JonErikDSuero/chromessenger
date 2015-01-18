47af27a104d9aabcaed5324cc72524489b4a5e5c
Jaime Barriga <jab.colo33@gmail.com>
2015-01-17 19:23:26 -0500

    magic

--- background/background.js
+++ background/background.js
@@ -2,6 +2,7 @@
 chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
 	if(request.method == "setLocalStorage"){
 		//localStorage["value1"] = request.value;
+		//console.log("Recieved message");
 		sendResponse({data: true});
 	} else {
 		sendResponse({});
